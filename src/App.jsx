6fc48dace12795199e58747609a337c76bffc52b
Salif Saidou <104588234+SAIDOU11@users.noreply.github.com>
2023-08-25 15:44:42 +0200

    useEffect practice

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,10 @@
-import { useState } from 'react';
+import WindowTracker from './WindowTracker.jsx';
 
 const App = () => {
-  const [starWarData, setData] = useState({});
-
-  // 1. GET the data (fetch)
-  // 2. Save the data state
-
-  fetch('https://swapi.dev/api/people/1')
-    .then((res) => res.json())
-    .then((data) => {
-      console.log(data);
-    });
-
   return (
-    <div>
-      <pre>{JSON.stringify(starWarData, null, 2)}</pre>
+    <div className="container">
+      <button>Toggle WindowTracker </button>
+      <WindowTracker />
     </div>
   );
 };
