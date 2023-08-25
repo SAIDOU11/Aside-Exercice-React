7ad3fa95bf4fd9d2c4a18383d896c2d2af3bbf0f
Salif Saidou <104588234+SAIDOU11@users.noreply.github.com>
2023-08-25 12:05:02 +0200

    api calls

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import { useState } from 'react';
 
 const App = () => {
-  const [data, setData] = useState({});
+  const [starWarData, setData] = useState({});
+
+  // 1. GET the data (fetch)
+  // 2. Save the data state
+
+  fetch('https://swapi.dev/api/people/1')
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+    });
+
   return (
     <div>
-      <pre>{JSON.stringify(data, null, 2)}</pre>
+      <pre>{JSON.stringify(starWarData, null, 2)}</pre>
     </div>
   );
 };
